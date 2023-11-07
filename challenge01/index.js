a36196dc082af564eaf07992ff60027b0afaf280
David Benavides <davirin96@gmail.com>
2023-11-06 19:59:07 -0600

    First challenge solution

---
+++ challenge01/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const port = 3000;
+
+
+app.get('/', (req, res) => {
+
+    const data = fs.readFileSync('./message_01.txt', 'utf8');
+    let words = [];
+    let result = "";
+
+    data.split(' ').forEach((word) => {
+        words[word.toLowerCase()] = words[word.toLowerCase()] ? words[word.toLowerCase()] + 1 : 1;
+    });
+
+    for (let word in words) {
+        result += `${word}${words[word]}`;
+    }
+
+    console.log(result);
+
+    res.send(result);
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
